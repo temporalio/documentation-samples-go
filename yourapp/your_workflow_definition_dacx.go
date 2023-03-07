5f7820a7ed7399c585adc30fdf7ff7b7b1e6424c
Cully Wakelin <cully@temporal.io>
2023-03-07 15:29:11 -0700

    updating comments

--- yourapp/your_workflow_definition_dacx.go
+++ yourapp/your_workflow_definition_dacx.go
@@ -8,17 +8,17 @@ import (
 
 /* The first parameter of a Go-based Workflow Definition must be of the [`workflow.Context`](https://pkg.go.dev/go.temporal.io/sdk/workflow#Context) type.
 It is used by the Temporal Go SDK to pass around Workflow Execution context, and virtually all the Go SDK APIs that are callable from the Workflow require it.
-It is acquired from the [`go.temporal.io/sdk/workflow`](https://pkg.go.dev/go.temporal.io/sdk/workflow) package. */
+It is acquired from the [`go.temporal.io/sdk/workflow`](https://pkg.go.dev/go.temporal.io/sdk/workflow) package.
 
-/* The `workflow.Context` entity operates similarly to the standard `context.Context` entity provided by Go.
-The only difference between `workflow.Context` and `context.Context` is that the `Done()` function, provided by `workflow.Context`, returns `workflow.Channel` instead of the standard Go `chan`. */
+The `workflow.Context` entity operates similarly to the standard `context.Context` entity provided by Go.
+The only difference between `workflow.Context` and `context.Context` is that the `Done()` function, provided by `workflow.Context`, returns `workflow.Channel` instead of the standard Go `chan`.
 
-/* The second parameter, `string`, is a custom parameter that is passed to the Workflow when it is invoked.
+The second parameter, `string`, is a custom parameter that is passed to the Workflow when it is invoked.
 A Workflow Definition may support multiple custom parameters, or none.
 These parameters can be regular type variables or safe pointers.
 However, the best practice is to pass a single parameter that is of a `struct` type, so there can be some backward compatibility if new parameters are added.
 
-/* All Workflow Definition parameters must be serializable and can't be channels, functions, variadic, or unsafe pointers. */
+All Workflow Definition parameters must be serializable and can't be channels, functions, variadic, or unsafe pointers. */
 
 // YourWorkflowParam is the object passed to the Workflow.
 type YourWorkflowParam struct {
