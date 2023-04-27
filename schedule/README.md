017e66e982a0df49d92aac75f74463c8a9860b7b
Samantha Santiago <samantha@temporal.io>
2023-04-27 11:58:26 -0500

    commit before merge

---
+++ schedule/README.md
@@ -0,0 +1,10 @@
+To run this sample, be sure to have a local development Cluster running.
+
+From the root of this package, start a Worker:
+
+From another terminal, start the Workflow Execution:
+
+Create Schedule:
+
+Backfill:
+
--- schedule/backfill_schedule_dacx.go
+++ schedule/backfill_schedule_dacx.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"go.temporal.io/sdk/client"
+)
+
 /*
  */
-func main() {
-
-}
+ func main() {
+	ctx := context.Background()
+	// The client is a heavyweight object that should be created once per process.
+	c, err := client.Dial(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+ }
