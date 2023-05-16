97fc0a2d294fd33584860d0cec96f7528c53727f
Samantha Santiago <samantha@temporal.io>
2023-05-16 12:13:35 -0500

    adding info to describe and pause

--- schedule/list/main.go
+++ schedule/list/main.go
@@ -8,6 +8,6 @@ func main() {
 id: how-to-list-a-schedule-in-go
 title: How to list a Schedule in Go
 label: List Schedules
-description: 
+description: List all Schedules in a Namespace in Go.
 lines: 
 @dacx */
