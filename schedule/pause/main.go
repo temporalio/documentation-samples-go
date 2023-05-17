cc6621ed5a6af26ae524e8f64786fbc072a6468a
Samantha Santiago <samantha@temporal.io>
2023-05-17 10:29:30 -0500

    pause info added

--- schedule/pause/main.go
+++ schedule/pause/main.go
@@ -31,20 +31,34 @@ func main() {
 			Workflow:  schedule.ScheduleWorkflow,
 			TaskQueue: "schedule",
 		},
+		Paused: true,
 	})
 	if err != nil {
 		log.Fatalln("Unable to create schedule", err)
 	}
 
-	// create verifystate variable
-	// pause and unpause
+	scheduleHandle.Unpause(ctx, client.ScheduleUnpauseOptions{
+		Note: "The Schedule has been unpaused.",
+	})
+	scheduleHandle.Pause(ctx, client.SchedulePauseOptions{
+		Note: "The Schedule has been paused.",
+	})
 }
 
+/*
+`Pause` and `Unpause` enable the start or stop of all future Workflow Runs on a given Schedule.
+
+Pausing a Schedule halts all future Workflow Runs.
+Pausing can be enabled by setting `State.Paused` to `true`, or by using `Pause()` on the ScheduleHandle.
+
+Unpausing a Schedule allows the Workflow to execute as planned.
+To unpause a Schedule, use `Unpause()` on `ScheduleHandle`.
+*/
 
 /* @dacx
 id: how-to-pause-a-schedule-in-go
 title: How to pause a Schedule in Go
 label: Pause Schedule
-description: 
-lines: 
+description: Show how to unpause and pause a Schedule in Go.
+lines: 12, 26, 34-35, 40-46
 @dacx */
