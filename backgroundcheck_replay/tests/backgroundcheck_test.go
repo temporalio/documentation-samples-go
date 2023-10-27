022f62819b284ef389cc633000dd9eb9192c41e4
Cully Wakelin <cully@temporal.io>
2023-10-27 13:25:07 -0600

    adding sleep

--- backgroundcheck_replay/tests/backgroundcheck_test.go
+++ backgroundcheck_replay/tests/backgroundcheck_test.go
@@ -70,7 +70,7 @@ func (s *UnitTestSuite) Test_SSNTraceActivity() {
 
 /*
 Add the Replay test to the set of application tests.
-The Replayer is available from the Worker package in the SDK.
+The Replayer is available from the `go.temporal.io/sdk/worker` package in the SDK.
 Register the Workflow Definition and then specify an existing Event History to compare to.
 
 Run the tests in the test directory (`go test`).
--- backgroundcheck_replay/workflows/backgroundcheck.go
+++ backgroundcheck_replay/workflows/backgroundcheck.go
@@ -10,6 +10,14 @@ import (
 
 // BackgroundCheck is your custom Workflow Definition.
 func BackgroundCheck(ctx workflow.Context, param string) (string, error) {
+	// Sleep for 1 minute
+	workflow.GetLogger(ctx).Info("Sleeping for 1 minute...")
+	// highlight-next-line
+	err := workflow.Sleep(ctx, 1*time.Minute)
+	if err != nil {
+		return err
+	}
+	workflow.GetLogger(ctx).Info("Finished sleeping")
 	// Define the Activity Execution options
 	// StartToCloseTimeout or ScheduleToCloseTimeout must be set
 	activityOptions := workflow.ActivityOptions{
@@ -18,6 +26,7 @@ func BackgroundCheck(ctx workflow.Context, param string) (string, error) {
 	ctx = workflow.WithActivityOptions(ctx, activityOptions)
 	// Execute the Activity synchronously (wait for the result before proceeding)
 	var ssnTraceResult string
+	// highlight-next-line
 	err := workflow.ExecuteActivity(ctx, activities.SSNTraceActivity, param).Get(ctx, &ssnTraceResult)
 	if err != nil {
 		return "", err
@@ -25,3 +34,15 @@ func BackgroundCheck(ctx workflow.Context, param string) (string, error) {
 	// Make the results of the Workflow available
 	return ssnTraceResult, nil
 }
+
+/* @dacx
+id: add-sleep-for-one-minute
+title: Add a call to sleep
+description: Add a call to sleep for one minute to the beginning of the Workflow.
+label: Add sleep call
+lines: 71-87
+tags:
+- testing
+- replay test
+- replayer
+@dacx */
